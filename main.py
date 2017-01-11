be1276647932806c523af0be4055c4f2f37f028b
Addison Elliott <addison.elliott@gmail.com>
2017-01-11 10:10:31 -0600

    Added TODO items and setup Python interpreter

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from mainWindow import *
 def main():
     app = QApplication(sys.argv)
 
+    # TODO Start by coding in the concept GUI to start out
+    # TODO Implement the buttons in the concept GUI
+
     form = MainWindow()
     form.show() # Uncomment this out when testing
     #form.showFullScreen() # Uncomment this when not testing
