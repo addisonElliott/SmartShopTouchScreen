c613193b9abef2b86d1502abea57d8aeaebd48a1
Addison Elliott <addison.elliott@gmail.com>
2017-03-16 07:45:28 -0500

    Added floating buttons on favorite menu for the plus/minus sign.

--- favoriteWindow.py
+++ favoriteWindow.py
@@ -23,3 +23,14 @@ class FavoriteWindow(QMainWindow, favoriteWindow_ui.Ui_FavoriteWindow):
         # Create shortcut for escape key that calls close()
         self.closeShortcut = QShortcut(Qt.Key_Escape, self)
         self.closeShortcut.activated.connect(self.close)
+
+        # Make the buttons at bottom of the screen floating; this cannot be done in Qt Designer
+        # Remove the widget containing the floating buttons from the layout since Qt Designer does not allow this
+        # Then setGeometry on the widget and finally raise the widget so it has a higher z-order than rest of items
+        self.centralwidget.layout().removeWidget(self.floatingBtnWidget)
+        floatingBtnWidgetWidth = 48 * 2 + 40
+        floatingBtnWidgetHeight = 48 + 12
+        floatingBtnWidgetX = (self.width() - floatingBtnWidgetWidth) / 2
+        floatingBtnWidgetY = self.height() - floatingBtnWidgetHeight - 1
+        self.floatingBtnWidget.setGeometry(floatingBtnWidgetX, floatingBtnWidgetY, floatingBtnWidgetWidth, floatingBtnWidgetHeight)
+        self.floatingBtnWidget.raise_()
