b8abe64f8ee4d0e549c309d9201c4628341428f2
Jacob Craft <jcrafty615@gmail.com>
2017-03-28 22:38:03 -0500

    fixed settings button

--- Windows/mainWindow.py
+++ Windows/mainWindow.py
@@ -39,13 +39,13 @@ class MainWindow(QWidget, mainWindow_ui.Ui_MainWindow):
     def on_ManualAddButton_clicked(self):
         self.parent().setCurrentIndex(WindowType.Favorites)
 
-#    @pyqtSlot()
-#    def on_SettingsButton_clicked(self):
-#        self.centralWindow.close()
     @pyqtSlot()
     def on_SettingsButton_clicked(self):
-        thing = ExpirationBox(self.config)
-        thing.exec()
+        self.centralWindow.close()
+#    @pyqtSlot()
+#    def on_SettingsButton_clicked(self):
+#        thing = ExpirationBox(self.config)
+#        thing.exec()
 
     @pyqtSlot()
     def scannerPoll_ticked(self):
